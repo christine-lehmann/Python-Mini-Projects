1c81f5340d89b6f4f84279496316c5ee4bccda4a
Christine Cion <64300985+christine-lehmann@users.noreply.github.com>
2021-08-14 16:04:36 +0800

    Funny Bunny Mad Lib

---
+++ Bunny Mad Lib.py
@@ -0,0 +1,28 @@
+def madlib():
+    per_name = input("Person's Name: ")
+    place = input("Place: ")
+    verb1 = input("Verb: ")
+    animal = input("Animal: ")
+    exer = input("An Exercise: ")
+    noun1 = input("Noun: ")
+    face = input("Part of Face: ")
+    adj1 = input("Adjective: ")
+    vege = input("Vegetable: ")
+    act = input("Activity: ")
+    verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
+    noun2 = input("Noun: ")
+    famous = input("Famous Person: ")
+
+    madlib = f"\nDear, {per_name}. \nI am writing this email to informed you that something funny happened at \
+{place}. \nWhen I was, {verb1} the trash out, I stumbled upon a {animal} doing push-ups and {exer}.\
+\nThis {noun1} was dressed in a bunny costume. \nIt had a large {face} and a cute {adj1} tail. \
+\nI asked this {vege} head what was he doing by the trash cans? \nHe replied, \"I'm training for the  \
+Easter Day {act}. \nRabbits always win, so I thought I would {verb2} like one and maybe finally win!\" \
+\nI said, \"Well Goodluck! I hope you {verb3} like the {noun2}.\" \
+\nYour Friend, {famous}. :)"
+
+    print(madlib)
+
+if __name__ == '__main__':
+    madlib()
