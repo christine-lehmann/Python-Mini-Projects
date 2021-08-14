66251c5438843b0ec322c2e2b247b978b80b409b
Christine Cion <64300985+christine-lehmann@users.noreply.github.com>
2021-08-14 17:22:33 +0800

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -12,7 +12,8 @@ def play(word):
     guessed_letters = []
     guessed_words = []
     tries = 6
-    print("Let's Play Hangman!")
+    name = input("What is your name? ")
+    print("Hello " + name + "! Let's Play Hangman!")
     print(display_hangman(tries))
     print(word_complete)
     print('\n')
